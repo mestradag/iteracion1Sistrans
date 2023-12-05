33601613804f7b0f19e13500b70563cf3e4a88b1
Maria Estrada <m.estradag@uniandes.edu.co>
2023-12-05 12:40:16 -0500

    Trate

--- src/main/java/uniandes/edu/co/proyecto/controller/CuentasConsumoController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/CuentasConsumoController.java
@@ -1,5 +1,7 @@
 package uniandes.edu.co.proyecto.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import uniandes.edu.co.proyecto.repositorio.CuentaConsumoRepository;
 import uniandes.edu.co.proyecto.modelo.CuentaConsumo;
 
@@ -20,14 +23,27 @@ public class CuentasConsumoController {
     private CuentaConsumoRepository cuentaconsumoRepository;
 
     @GetMapping("/cuentas_c")
-    public String cuentasConsumo(Model model, String nombreusuario, String fechainicio, String fechafin) {
+    public String cuentasConsumo(Model model, String fechainicio, String fechafin) {
+
+        System.out.println("Fecha Inicio: " + fechainicio);
+        System.out.println("Fecha Fin: " + fechafin);
 
         model.addAttribute("cuentas_c", cuentaconsumoRepository.findAll()); 
         if ((fechainicio == null || fechainicio.equals("")) && (fechafin == null || fechafin.equals(""))) {
             model.addAttribute("cuentas_c", cuentaconsumoRepository.findAll());  
         }
         else{
-            model.addAttribute("cuentas_c", cuentaconsumoRepository.consumoCliente(fechainicio, fechafin));
+
+            try {
+                Date fechaInicio = fechainicio != null ? new SimpleDateFormat("yyyy-MM-dd").parse(fechainicio) : null;
+                Date fechaFin = fechafin != null ? new SimpleDateFormat("yyyy-MM-dd").parse(fechafin) : null;
+    
+                System.out.println("Entro");
+                model.addAttribute("cuentas_c", cuentaconsumoRepository.consumoCliente(fechaInicio, fechaFin));
+            } catch (ParseException e) {
+                // Handle the date parsing exception (e.g., log or show an error message)
+                e.printStackTrace();
+            }
         }
         return "cuentas_c";
 
--- src/main/java/uniandes/edu/co/proyecto/repositorio/CuentaConsumoRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/CuentaConsumoRepository.java
@@ -6,20 +6,19 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CuentaConsumoRepository extends MongoRepository<CuentaConsumo, String> {
 
     //RFC3 -  MOSTRAR EL CONSUMO EN HOTELANDES POR UN CLIENTE, EN UN RANGO DE FECHAS INDICADO. 
     //Recuerde que un cliente puede alojarse en el hotel cuantas veces quiera.
-
     @Aggregation(pipeline = {
-        "{$match: {'reservas.cuenta_c.checkin': { $gte: { $dateFromString: { dateString: ':?0', format: '%Y-%m-%dT%H:%M:%S.%LZ' } }, $lte: { $dateFromString: { dateString: ':?1', format: '%Y-%m-%dT%H:%M:%S.%LZ' } } } }}",
-        "{$match: {rol:'cliente'}}",
+        "{$match: {'reservas.cuenta_c.checkin': { $gte: ?0, $lte: ?1 } }}",
+        "{$match: {rol: 'cliente'}}",
         "{$lookup: {from: 'servicios', localField: 'reservas.cuenta_c.serviciosconsumidos.idservicio', foreignField: '_id', as: 'servicio'}}",
         "{$unwind: '$servicio'}",
-        "{$group: {_id: {idcuenta: '$idcuenta', nombreCliente: '$nombre'}, totalConsumo: {$sum: '$servicio.costototal'}}}",
-        "{$project: {idcuenta: '$_id.idcuenta', nombreCliente: '$_id.nombreCliente', totalConsumo: 1, rol:1, _id: 0}}"
+        "{$group: {_id: {idcuenta: '$idcuenta', nombreCliente: '$nombre'}, totalConsumo: {$sum: '$servicio.costototal'}}}"
     })
-    List<CuentaConsumo> consumoCliente(String fechaInicio, String fechaFin);
+    List<CuentaConsumo> consumoCliente(@Param("0") Date fechainicio,  @Param("1") Date fechafin);
 }
 
